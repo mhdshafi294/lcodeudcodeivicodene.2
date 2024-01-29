68ab84b8f22dd688beb4db15d9954176f3dda479
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2024-01-29 08:49:20 +0300

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,48 @@
+export const NAV_LINKS = [
+  {
+    label: "Necklaces",
+    value: "/necklaces" as const,
+  },
+  {
+    label: "Earrings",
+    value: "/earrings" as const,
+  },
+  {
+    label: "Rings",
+    value: "/rings" as const,
+  },
+  {
+    label: "Bracelets",
+    value: "/bracelets" as const,
+  },
+  {
+    label: "Our Story",
+    value: "/our-Story" as const,
+  },
+];
+
+export const HERO_IMGS = [
+  {
+    label: "hero1",
+    value: "/src/assets/hero.png" as const,
+  },
+];
+
+export const CATEGORIES = [
+  {
+    label: "Rings",
+    value: "/src/assets/hero.png",
+  },
+  {
+    label: "Bracelets",
+    value: "/src/assets/hero.png",
+  },
+  {
+    label: "Necklaces",
+    value: "/src/assets/hero.png",
+  },
+  {
+    label: "Earrings",
+    value: "/src/assets/hero.png",
+  },
+];
